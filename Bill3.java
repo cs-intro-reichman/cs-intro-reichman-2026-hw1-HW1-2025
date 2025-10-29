1546d62e003dbbf58a1209e471fb7f13bb5782d0
ReutHaimi <reut.haimi@post.runi.ac.il>
2025-10-29 19:09:15 +0200

    Splitting the bill for 3 diners at a restaurant - new project

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,12 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int total_amount = Integer.parseInt(args[3]);
+
+		double amount_per_person = Double.valueOf(total_amount) / (args.length - 1);
+		amount_per_person = Math.ceil(amount_per_person) ; 
+		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 + ": pay " + amount_per_person + " Shekels each.");
 	}
 }
