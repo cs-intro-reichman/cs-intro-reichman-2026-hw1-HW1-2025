23b2632463dadcb5b1918d2f020a9c09ff8183b5
ReutHaimi <reut.haimi@post.runi.ac.il>
2025-10-31 00:12:07 +0200

    printing time in a different format

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,10 +1,6 @@
 // Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
 	public static void main(String[] args) {
-		// In Java, the command-line arguments args[0], args[1], ... are represented
-		// each by a string. In this program, the single "hh:mm" input is represented
-		// by the single command-line string argument args[0]. 
-		//   
 		// The following statement handles the hours part of the input.
 		// It concatenates the empty string "" with the leftmost hour-digit. 
 		// It then concatenates the resulting string with the rightmost hour-digit,
@@ -12,6 +8,29 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+		System.out.println(minutes);
+
+		String fullMinutes = Integer.toString(minutes);
+		Boolean pm = false;
+		//Checks if the time is greater than 12
+		if (hours > 12)
+		{
+			hours = hours - 12;
+			pm = true;
+		}
+		//Checks if the minutes are a single digit to add 0
+		if (minutes < 10)
+		{
+		fullMinutes = "0" + minutes;	
+		}
+		//Prints the time according to pm and am
+		if (pm == true){
+		System.out.println( hours + ":" + fullMinutes + " PM");
+		}
+		else {
+		System.out.println( hours + ":" + fullMinutes + " AM");	
+		}
+	
 	}
+
 }
