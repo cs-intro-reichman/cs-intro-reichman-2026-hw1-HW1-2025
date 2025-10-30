8ddea891b3018e448a2c337352818f9a92ebd076
ReutHaimi <reut.haimi@post.runi.ac.il>
2025-10-30 23:14:11 +0200

    calculate fix

--- FVCalc.java
+++ FVCalc.java
@@ -4,10 +4,10 @@ public class FVCalc {
 		double currentValue = Integer.parseInt(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
-		double futureValue = currentValue * (Math.pow((1 + rate),n))/100;
+		double futureValue = currentValue * (Math.pow((1 + rate/100),n));
 
-		System.out.println("After " + (int)n + " years, $" + (int)currentValue + " saved at " + rate + "% will yield " + (int)futureValue + "$");
+		System.out.println("After " + (int)n + " years, $" + (int)currentValue + " saved at " + rate + "% will yield $" + (int)futureValue);
 
-		//להבין למה יצא לי מספר לא טוב בערך העתידי
+	
 	}
 }
