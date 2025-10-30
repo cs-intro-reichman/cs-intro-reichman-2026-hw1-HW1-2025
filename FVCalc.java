8cec5a4829b4f8c35ade4d07cc7ec7946c553e88
ReutHaimi <reut.haimi@post.runi.ac.il>
2025-10-30 22:52:10 +0200

    New project - Arranging random numbers from smallest to largest

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,13 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		double currentValue = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int n = Integer.parseInt(args[2]);
+		double futureValue = currentValue * (Math.pow((1 + rate),n))/100;
+
+		System.out.println("After " + (int)n + " years, $" + (int)currentValue + " saved at " + rate + "% will yield " + (int)futureValue + "$");
+
+		//להבין למה יצא לי מספר לא טוב בערך העתידי
 	}
 }
